9c3d5ed15cd4bfa10cff572fdc4cc1eeb7202eb3
andrew <564631192@qq.com>
2025-03-06 23:12:29 +0800

    commit

--- convey/convey_test.go
+++ convey/convey_test.go
@@ -10,7 +10,6 @@ func TestSpec(t *testing.T) {
 	// Only pass t into top-level Convey calls
 	Convey("Given some integer with a starting value", t, func() {
 		x := 1
-
 		Convey("When the integer is incremented", func() {
 			x++
 
