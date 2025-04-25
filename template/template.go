ed76424bacdf983c030ae74996e0e1189c053823
wangyazhou <yazhou.wang.hytera.com>
2025-04-25 20:22:21 +0800

    refactor(import): 更新项目依赖包路径
    - 将 "github.com/zzu-andrew/go-example/gin_master/gin" 替换为 "go-example/gin_master/gin"
    - 更新相关测试文件中的导入路径
    - 修改 main.go 中的导入路径

--- template/template.go
+++ template/template.go
@@ -1,4 +1,6 @@
-package template
+package main
+
+import "fmt"
 
 /*
 K 和 V 你可以理解为类型别名，在中括号之间进行定义，作用域也只在此函数内，可以在形参、函数主体、返回值类型 里使用
@@ -14,4 +16,23 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 		s += v
 	}
 	return s
+}
+
+func main() {
+	// map[string]int64
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+	// map[string]float64
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	return
 }
