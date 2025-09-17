26aa4b58be9c7262ea7a96f09b934f9fb87a496c
wangyazhou <564631192@qq.com>
2025-09-17 22:42:29 +0800

    refactor(gRPC): 重构 gRPC 客户端代码
    - 添加命令行参数解析
    - 使用 insecure 包进行不安全连接- 优化变量命名和代码结构
    - 更新日志输出内容

--- gRPC/sendmessage/client.go
+++ gRPC/sendmessage/client.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/credentials/insecure"
+
 	// 导入grpc包
 	"google.golang.org/grpc"
 	// 导入刚才我们生成的代码所在的proto包。
@@ -16,28 +19,21 @@ const (
 )
 
 func main() {
-	// 连接grpc服务器
-	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	// 延迟关闭连接
 	defer conn.Close()
-
-	// 初始化Greeter服务客户端
 	c := pb.NewSendMessageClient(conn)
 
-	// 初始化上下文，设置请求超时时间为1秒
+	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	// 延迟关闭请求会话
 	defer cancel()
-
-	// 调用SayHello接口，发送一条消息
-	r, err := c.SendMessage(ctx, &pb.Request{Name: "world"})
+	r, err := c.SendMessage(ctx, &pb.Request{Name: "Send message"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-
-	// 打印服务的返回的消息
-	log.Printf("Greeting: %s", r.Message)
+	log.Printf("Greeting: %s", r.GetMessage())
 }
