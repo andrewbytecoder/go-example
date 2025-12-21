9165603ba9baca2fcb9e49bfb1f25d483c5ce470
wangyazhou <564631192@qq.com>
2025-12-21 16:19:42 +0800

    chore: 更新项目配置文件和文档
    - 修改 .gitignore 文件排除不必要的文件
    - 更新 .golangci.yml 配置优化代码检查
    - 调整 .travis.yml 持续集成配置
    - 更新 CHANGELOG.md 记录版本变更
    - 修改 CODE_OF_CONDUCT.md 行为准则文件
    - 更新 CONTRIBUTING.md 贡献指南文档
    - 修改多个源码文件完善功能实现
    - 更新 exampledata.txt 示例数据文件

--- exporter/example.go
+++ exporter/example.go
@@ -19,14 +19,8 @@ import (
 	"regexp"
 	"sync"
 
-	"github.com/alecthomas/kingpin/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"go.uber.org/zap"
-)
-
-var (
-	netclassIgnoredDevices = kingpin.Flag("collector.netclass.ignored-devices", "Regexp of net devices to ignore for netclass collector.").Default("^$").String()
-	netclassInvalidSpeed   = kingpin.Flag("collector.netclass.ignore-invalid-speed", "Ignore devices where the speed is invalid. This will be the default behavior in 2.x.").Bool()
 )
 
 type netClassCollector struct {
@@ -43,7 +37,7 @@ func init() {
 
 // NewNetClassCollector returns a new Collector exposing network class stats.
 func NewNetClassCollector(logger *zap.Logger) (Collector, error) {
-	pattern := regexp.MustCompile(*netclassIgnoredDevices)
+	pattern := regexp.MustCompile("^$")
 	return &netClassCollector{
 		subsystem:             "network",
 		ignoredDevicesPattern: pattern,
@@ -68,41 +62,7 @@ func (c *netClassCollector) netClassSysfsUpdate(ch chan<- prometheus.Metric) err
 
 	ch <- prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, upValue, "eth0")
 
-	infoDesc := prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, c.subsystem, "info"),
-		"Non-numeric data from /sys/class/net/<iface>, value is always 1.",
-		[]string{"device", "address", "broadcast", "duplex", "operstate", "adminstate", "ifalias"},
-		nil,
-	)
-	infoValue := 1.0
-
-	ch <- prometheus.MustNewConstMetric(infoDesc, prometheus.GaugeValue, infoValue, ifaceInfo.Name, ifaceInfo.Address, ifaceInfo.Broadcast, ifaceInfo.Duplex, ifaceInfo.OperState, getAdminState(ifaceInfo.Flags), ifaceInfo.IfAlias)
-
-	pushMetric(ch, c.getFieldDesc("address_assign_type"), "address_assign_type", ifaceInfo.AddrAssignType, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("carrier"), "carrier", ifaceInfo.Carrier, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("carrier_changes_total"), "carrier_changes_total", ifaceInfo.CarrierChanges, prometheus.CounterValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("carrier_up_changes_total"), "carrier_up_changes_total", ifaceInfo.CarrierUpCount, prometheus.CounterValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("carrier_down_changes_total"), "carrier_down_changes_total", ifaceInfo.CarrierDownCount, prometheus.CounterValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("device_id"), "device_id", ifaceInfo.DevID, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("dormant"), "dormant", ifaceInfo.Dormant, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("flags"), "flags", ifaceInfo.Flags, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("iface_id"), "iface_id", ifaceInfo.IfIndex, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("iface_link"), "iface_link", ifaceInfo.IfLink, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("iface_link_mode"), "iface_link_mode", ifaceInfo.LinkMode, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("mtu_bytes"), "mtu_bytes", ifaceInfo.MTU, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("name_assign_type"), "name_assign_type", ifaceInfo.NameAssignType, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("net_dev_group"), "net_dev_group", ifaceInfo.NetDevGroup, prometheus.GaugeValue, ifaceInfo.Name)
-
-	if ifaceInfo.Speed != nil {
-		// Some devices return -1 if the speed is unknown.
-		if *ifaceInfo.Speed >= 0 || !*netclassInvalidSpeed {
-			speedBytes := int64(*ifaceInfo.Speed * 1000 * 1000 / 8)
-			pushMetric(ch, c.getFieldDesc("speed_bytes"), "speed_bytes", speedBytes, prometheus.GaugeValue, ifaceInfo.Name)
-		}
-	}
-
-	pushMetric(ch, c.getFieldDesc("transmit_queue_length"), "transmit_queue_length", ifaceInfo.TxQueueLen, prometheus.GaugeValue, ifaceInfo.Name)
-	pushMetric(ch, c.getFieldDesc("protocol_type"), "protocol_type", ifaceInfo.Type, prometheus.GaugeValue, ifaceInfo.Name)
+	pushMetric(ch, c.getFieldDesc("address_assign_type"), "address_assign_type", "type", prometheus.GaugeValue, "eth0")
 
 	return nil
 }
